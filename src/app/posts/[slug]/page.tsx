3950bef9723d01f19d0a946a507a1019fa051af9
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-12-22 00:37:01 +0900

    12/22 posts 상세페이지 이전 데이터 다음 데이터 셋 ! (index로 필터 후 오브젝트에 데이터 추가)

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import MarkdownViewer from '@/components/MarkdownViewer';
+
+import PostContent from '@/components/PostContent';
 import { getPostData } from '@/service/posts'
 import Image from 'next/image';
-import { FaRegCalendar } from "react-icons/fa";
 import React from 'react'
 
 type Props = {
@@ -11,7 +11,8 @@ type Props = {
 }
 
 export default async function Postpage({ params : {slug}} :Props) {
-    const {title, description, date, path, content} = await getPostData(slug);
+    const post = await getPostData(slug);
+    const {title, path, next, prev} = post;
 
     return (
         <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
@@ -21,15 +22,11 @@ export default async function Postpage({ params : {slug}} :Props) {
             alt={title} 
             width={760} 
             height={420} />
-            <section className='flex flex-col p-4'>
-                <div className='flex items-center self-end text-sky-600'>
-                <FaRegCalendar />
-                <p className='font-semibold ml-2'>{date.toString()}</p>
-                </div>
-                <h1 className='text-4xl font-bold'>{title}</h1>
-                <p className='text-xl font-bold'>{description}</p>
-                <div className='w-44 border-2 border-sky-600 mt-4 mb-8' />
-                <MarkdownViewer content={content} />
+            
+        <PostContent post={post} />
+            <section>
+                {prev && <p>{prev.title}</p>}
+                {next && <p>{next.title}</p>}
             </section>
         </article>
     )
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,7 +10,12 @@ export type Post = {
     featured : boolean,
 };
 
-export type PostData = Post & {content : string};
+export type PostData = Post & 
+{
+    content : string, 
+    next : Post | null, 
+    prev : Post | null 
+};
 
 export async function getNonFeaturedPosts(): Promise<Post[]> {
     return getAllPosts() // featured가 true인 것만 필터
@@ -33,10 +38,17 @@ export async function getAllPosts(): Promise<Post[]> {
 // url fileName과 같은 데이터 필터 및 url과 같은 .md 파일 데이터도 함께 가져옴
 export async function getPostData(fileName:string): Promise<PostData> {
     const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
-    const metadata = await getAllPosts()
-    .then(posts => posts.find(post => post.path === fileName));
-    if(!metadata) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+    const posts = await getAllPosts();
+    const post = posts.find(post => post.path === fileName);
+
+    if(!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+    // 인덱스 값으로 배열의 첫번 째를 제외한 데이터의 이전, 다음을 가져옴
+    const index = posts.indexOf(post);
+    const next = index > 0 ? posts[index-1] : null;
+    const prev = index < posts.length-1 ? posts[index + 1] : null
 
     const content = await readFile(filePath, 'utf-8');
-    return {...metadata, content};
+    return {...post, content, next, prev };
 }
