f4e641d299d0d859523a1b7610ea1f2df12fc2c8
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-12-25 21:37:39 +0900

    12/25 이메일 폼 컴포넌트 클라이언트 컴포넌트 셋

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,24 @@
+import Hero from '@/components/Hero'
 import React from 'react'
 
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
 export default function AboutPage() {
     return (
         <div>
-            나의 대해
+            <Hero />
+            <section className='bg-gray-100 shadow-lg p-8 m-8 text-center'>
+                <h2 className={TITLE_CLASS}>Who Am I ?</h2>
+                <p>개발을 사랑하는 풀스택 개발자 <br />
+                사람과 디자인을 담는 웹앱을 만들고 있음</p>
+                <h2 className={TITLE_CLASS}>Career </h2>
+                <p>구글러 (-Now) <br />
+                메이스북 (-2019) <br /></p>
+                <h2 className={TITLE_CLASS}>Skills</h2>
+                <p>React, Vue, Node <br />
+                Git, Clean Code <br />
+                Vs Code, Intellig, MongoDB</p>
+            </section>
+            
         </div>
     )
 }
