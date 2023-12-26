c31a962fa314efe2ca947b5d35aef0a8a3cd889d
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-12-26 22:05:56 +0900

    12/26 배너 및 이메일 보내기 Yup, 노드메일러를 이용한 POST 요청

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,15 @@
+import * as yup from 'yup';
+
+const bodySchema = yup.object().shape({
+    from : yup.string().email().required(),
+    subject : yup.string().required(),
+    message : yup.string().required(),
+})
+
+export async function POST(req : Request) {
+    if(!bodySchema.isValidSync(req.body)) {
+        return new Response('유효하지 않는 포맷', {status : 400});
+    }
+    const { from, subject, message } = req.body;
+    // 노므메일러를 이용해서 메일을 전송하면 됨
+}
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,7 @@
 'use client'
 import React, { ChangeEvent, FormEvent, useState } from 'react'
+import Banner, { BannerData } from './Banner';
+import { sendContactEmail } from '@/service/contact';
 
 type Form = {
     from : string,
@@ -7,52 +9,85 @@ type Form = {
     message : string,
 }
 
+const DEFAULT_DATA = {
+    from : '',
+    subject : '',
+    message : '',
+};
+
 export default function ContactForm() {
-    const [form, setForm] = useState<Form>({
-        from : '',
-        subject : '',
-        message : '',
-    });
+    const [form, setForm] = useState<Form>(DEFAULT_DATA);
+    const [banner, setBanner] = useState<BannerData | null>(null);
+
     const onChange = (e : ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const {name, value} = e.target;
         setForm((prev) => ({...prev, [name] : value}))
     }
     const onSubmit = (e : FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(form)
+        sendContactEmail(form)
+        .then(()=>{
+            setBanner({message : '메일을 성공적으로 보냈습니다', state : 'success'});
+            setForm(DEFAULT_DATA);
+        })
+        .catch(()=> {
+            setBanner({message : '메일 전송을 실패했습니다', state : 'error'});
+        })
+        .finally(()=>{
+            setTimeout(()=> {
+                setBanner(null);
+            }, 3000)
+        });
+        
+        
     }
 
     return (
-        <>
-            <form onSubmit={onSubmit}>
-                <label htmlFor='from'>Your Email</label>
+        <section className='w-full max-w-md'>
+        {banner && <Banner banner={banner} />}
+            <form onSubmit={onSubmit}
+            className='w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white'
+            >
+                <label
+                className='font-semibold'
+                htmlFor='from'>Your Email</label>
                 <input 
                 type="email" 
                 id='from' 
                 name='from' 
                 required autoFocus 
                 value={form.from} 
-                onChange={onChange}/>
-
-                <label htmlFor='subject'>Subject</label>
+                onChange={onChange}
+                className='text-black'
+                />
+                
+                <label
+                className='font-semibold'
+                htmlFor='subject'>Subject</label>
                 <input 
                 type="text" 
                 id='subject' 
                 name='subject' 
                 required 
                 value={form.subject} 
-                onChange={onChange}/>
-
-            <label htmlFor='message'>Message</label>
+                onChange={onChange}
+                className='text-black'
+                />
+                
+            <label 
+            className='font-semibold'
+            htmlFor='message'>Message</label>
                 <textarea 
                 rows={10}
                 id='message' 
                 name='message' 
                 required 
                 value={form.message} 
-                onChange={onChange}/>
-                <button>Submit</button>
+                onChange={onChange}
+                className='text-black'
+                />
+                <button className='bg-yellow-300 text-black font-bold hover:bg-yellow-400'>Submit</button>
             </form>
-        </>
+        </section>
     )
 }
