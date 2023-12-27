26c4c0712692d8758cbf294d079326b455ba110f
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-12-27 22:10:55 +0900

    12/27 nodemailer를 사용한 서버 라우팅 및 POST 요청

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,15 +1,30 @@
+import { sendEmail } from '@/service/email';
 import * as yup from 'yup';
 
 const bodySchema = yup.object().shape({
-    from : yup.string().email().required(),
-    subject : yup.string().required(),
-    message : yup.string().required(),
-})
+    from: yup.string().email().required(),
+    subject: yup.string().required(),
+    message: yup.string().required(),
+    });
 
-export async function POST(req : Request) {
-    if(!bodySchema.isValidSync(req.body)) {
-        return new Response('유효하지 않는 포맷', {status : 400});
+    export async function POST(req: Request) {
+    const body = await req.json();
+    if (!bodySchema.isValidSync(body)) {
+        return new Response(JSON.stringify({ message: '메일 전송에 실패함!' }), {
+        status: 400,
+        });
     }
-    const { from, subject, message } = req.body;
-    // 노므메일러를 이용해서 메일을 전송하면 됨
+    return sendEmail(body) //
+        .then(
+        () =>
+            new Response(JSON.stringify({ message: '메일을 성공적으로 보냈음' }), {
+            status: 200,
+            })
+        )
+        .catch((error) => {
+        console.error(error);
+        return new Response(JSON.stringify({ message: '메일 전송에 실패함!' }), {
+            status: 500,
+        });
+        });
 }
